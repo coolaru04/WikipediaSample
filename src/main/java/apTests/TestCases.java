32b566e843edf0a064ad090527c69dfe1a4cc136
coolaru04 <coolaru04@gmail.com>
2025-03-29 21:32:25 +0530

    Update TestCases.java

--- src/main/java/apTests/TestCases.java
+++ src/main/java/apTests/TestCases.java
@@ -1,38 +1,38 @@
 
 package apTests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 //Selenium Imports
 import java.util.logging.Level;
+import org.apache.hc.client5.http.impl.routing.SystemDefaultRoutePlanner;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.BrowserType;
 ///
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TestCases {
-    WebDriver driver;
+    RemoteWebDriver driver;
 
-    public TestCases() {
+    public TestCases() throws MalformedURLException {
         System.out.println("Constructor: TestCases");
 
-        WebDriverManager.chromedriver().timeout(30).setup();
-        ChromeOptions options = new ChromeOptions();
-        LoggingPreferences logs = new LoggingPreferences();
+        final DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(BrowserType.CHROME);
+        driver = new RemoteWebDriver(new URL("http://localhost:8082/wd/hub"), capabilities);
 
-        // Set log level and type
-        logs.enable(LogType.BROWSER, Level.ALL);
-        logs.enable(LogType.DRIVER, Level.ALL);
-        options.setCapability("goog:loggingPrefs", logs);
-
-        // Set path for log file
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "chromedriver.log");
-
-        driver = new ChromeDriver(options);
 
         // Set browser to maximize and wait
         driver.manage().window().maximize();
@@ -47,11 +47,187 @@ public class TestCases {
 
     }
 
-    public void testCase01() {
+    public void testCase01() throws InterruptedException {
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase01");
+        driver.get("https://www.wikipedia.org/");
+
+        String currentURL = driver.getCurrentUrl();
+        if(currentURL.contains("wikipedia")){
+            System.out.println("The current URL contains the expected title wikipedia");
+            System.out.println("Testcase01 passed");
+        }
+        else{           
+            System.out.println("Testcase01 Fail");
+        }   
+        System.out.println("End Test case: testCase01");
     }
 
-}
 
+    public void testCase02() throws InterruptedException {
+        System.out.println("Start Test case: testCase02");
+        
+        WebElement header=driver.findElement(By.xpath("//*[@class='central-textlogo__image sprite svg-Wikipedia_wordmark']"));
+        if (header.getText().contains("Wikipedia")){
+            System.out.println("The header text contains Wikipedia");
+        }
+        else{
+            System.out.println("Header text doesn't contain Wikipedia");
+        }
+        
+        WebElement footertext1=driver.findElement(By.partialLinkText("Terms of Use"));
+        if(footertext1.isDisplayed()){
+            System.out.println("The footer link Terms of Use is displayed");
+        }
+        else{
+            System.out.println("The footer link Terms of Use is NOT displayed");
+        }
+
+        WebElement footertext2=driver.findElement(By.partialLinkText("Privacy Policy"));
+        if(footertext2.isDisplayed()){
+            System.out.println("The footer link  Privacy Policy is displayed");
+            System.out.println("Test case02 pass");
+        }
+        else{
+            System.out.println("The footer link Privacy Policy is NOT displayed");
+            System.out.println("Test case02 fail");
+        }
+  
+        System.out.println("End Test case: testCase02");
+    }
+
+    public void testCase03() throws InterruptedException {
+        System.out.println("Start Test case: testCase03");
+        
+        WebElement Searchbar=driver.findElement(By.xpath("//input[@id='searchInput']"));
+        Searchbar.sendKeys("apple");
+        boolean flag1=false;
+        List<WebElement> Searchresult=driver.findElements(By.xpath("//*[@id='typeahead-suggestions']/div/a"));
+        for(WebElement apple:Searchresult){             
+            if(apple.getAttribute("href").contains("https://en.wikipedia.org/wiki/Apple_Inc.")){                                       
+                apple.click(); 
+                flag1=true;
+                break;
+            }
+        }
+        if(flag1){
+            System.out.println("The apple inc link is clicked");
+        }
+        else{
+            System.out.println("The apple inc link is NOT clicked");
+        }  
+  
+        
+        WebElement founder=driver.findElement(By.xpath("//*[@id='mw-content-text']/div[1]/table[1]/tbody/tr[9]/th"));
+        String checkfounder=founder.getText();
+        boolean flag=false;
+        if(checkfounder.contains("Founders")){
+            List<WebElement> founderlist=driver.findElements(By.xpath("//*[@id='mw-content-text']/div[1]/table[1]/tbody/tr[9]/td/div/ul/li"));
+            for(WebElement element:founderlist){
+                if(element.getText().contains("Steve Jobs"))
+                {
+                    flag=true;                    
+                    break;
+                }          
+            }
+            
+        }
+        else{
+            System.out.println("Founder is not present in the page");        
+        }
+
+        if(flag){
+            System.out.println("The Steve Jobs is present as a Founder");
+        }
+        else{
+            System.out.println("The Steve Jobs is NOT present as a Founder");
+        }
+
+        System.out.println("End Test case: testCase03");
+    }
+    public void testCase04() throws InterruptedException {
+            System.out.println("Start Test case: testCase04");
+            driver.get("https://www.wikipedia.org/");
+            
+            WebElement Searchbar=driver.findElement(By.xpath("//input[@id='searchInput']"));
+            Searchbar.sendKeys("microsoft");
+            boolean flag1=false;
+            List<WebElement> Searchresult=driver.findElements(By.xpath("//*[@id='typeahead-suggestions']/div/a"));
+            for(WebElement microsoft:Searchresult){             
+                if(microsoft.getAttribute("href").contains("https://en.wikipedia.org/wiki/Microsoft")){                                       
+                    microsoft.click(); 
+                    flag1=true;
+                    break;
+                }
+            }
+       
+            if(flag1){
+                System.out.println("Microsoft link is found and clicked");
+            }
+            else{
+                System.out.println("Microsoft link is NOT found");
+            }
+                           
+        List<WebElement> founder=driver.findElements(By.xpath("//*[@id='mw-content-text']/div[1]/table[1]/tbody/tr/th"));        
+        boolean flag=false;
+       
+
+        for(WebElement checkfounder:founder){
+            boolean flag2=false;
+            if(checkfounder.getText().contains("Founders")){
+                List<WebElement> foundername=driver.findElements(By.xpath("//*[@id='mw-content-text']/div[1]/table[1]/tbody/tr[8]/td/div/ul/li/a"));
+                for(WebElement checkfoundername:foundername){
+                    if(checkfoundername.getAttribute("title").contains("Bill Gates")){                                           
+                        flag=true;
+                        checkfoundername.click();
+                        flag2=true;
+                        break;
+                    }
+                }
+
+            } 
+            if(flag2) {
+                break;
+            }
+           
+        }
+       
+        if(flag){
+            System.out.println("The Bill Gates is present as a Founder");
+        }
+        else{
+            System.out.println("The SBill Gates is NOT present as a Founder");
+        }
+
+        String currentURL=driver.getCurrentUrl();
+        if(currentURL.contains("/Bill_Gates")){
+            System.out.println("Bill Gates is found in current URL");
+            System.out.println("Testcase04 Pass");
+        }
+        else{
+            System.out.println("Bill Gates is NOT found in current URL");
+            System.out.println("Testcase04 Fail");
+        }       
+    }
+
+    public void testCase05() throws InterruptedException {
+        System.out.println("Start Test case: testCase05");
+        
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");               
+        Thread.sleep(3000);
+        WebElement Mainmenu=driver.findElement(By.id("vector-main-menu-dropdown-checkbox"));
+        Mainmenu.click();
+        WebElement AboutWiki=driver.findElement(By.linkText("About Wikipedia"));
+        AboutWiki.click();
+        String currentURL=driver.getCurrentUrl();
+        if(currentURL.contains("About")){
+            System.out.println("The current URL contains About");   
+            System.out.println("Testcase05 pass");     
+        }
+        else{
+            System.out.println("The current URL does not contain About");                    
+            System.out.println("Testcase05 Fail");     
+        }
+        System.out.println("End Test case: testCase05");
+ } 
+ 
+}
